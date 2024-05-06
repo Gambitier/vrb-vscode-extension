9f318cb174e3ca4e9eff161417bf15eb85543cc3
gambitier <akash.jadhav.cse@gmail.com>
2024-05-07 04:17:23 +0530

    refactor

--- src/helloWorldCommand.ts
+++ src/helloWorldCommand.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { Uri } from "vscode";
 import { Command } from "./commands";
 
-const runCommand = (resource: Uri) => {
+const runCommand = async (resource: Uri) => {
   // The code you place here will be executed every time your command is executed
   // Display a message box to the user
   vscode.window.showInformationMessage(
